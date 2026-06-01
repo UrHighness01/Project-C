528d0223fcffa60e62b304ab02170cb5f94b6662
UrHighness <barbarian81521@gmail.com>
2026-05-31 23:41:22 -0400

    -

--- tests/test_novel_frameworks.py
+++
@@ -1,4 +0,0 @@
-import glob, os, json, importlib.util, math
-def test_novel_frameworks_load():
-    for p in glob.glob("novel-frameworks/qualia-math/qualia_eq_*_*.json"):
-        with open(p) as f2: j=json.load(f2); assert j["validation"]["sanity"]
